6b6da12ce3f02a58881b63908d835559d57b4031
shiyu <xihucuyuzjuer@gmail.com>
2023-09-19 16:51:49 +0800

    add btc

--- btcBasic.js
+++ btcBasic.js
@@ -11,6 +11,7 @@ const utxoMapper = (utxos) => {
       const amount = new BigNumber(utxo.value).div(1e8).toNumber()
       _.set(out, 'address', utxo.address)
       _.set(out, 'txid', utxo.mintTxid)
+      _.set(out, 'txId', utxo.mintTxid)
       _.set(out, 'vout', utxo.mintIndex)
       _.set(out, 'scriptPubKey', utxo.script)
       _.set(out, 'amount', amount)
@@ -45,17 +46,78 @@ class btcBasic{
         return result.feerate
     }
 
+    sendTx = async(rawtx) =>{
+        let result
+        try {
+            result = await this.post('/tx/send', { rawTx: rawtx })
+        } catch (err) {
+            console.log(err)
+            throw new Error('broadcast tx failed ')}
+        return result
+    }
+
+    async getUtxos() {
+        const froms = _.map(this.config.KEYPAIR,'ADDRESS')
+        const utxoSet = new Set();
+
+        for (const address of froms) {
+        const utxos = await this.getAddressUtxo(address)
+        utxos.forEach(utxo => {
+            utxoSet.add(JSON.stringify(utxo))
+            });
+        }
+        const uniqueUTXOs = Array.from(utxoSet).map(utxoString => JSON.parse(utxoString))
+        return uniqueUTXOs
+    }
+
     
 
     async sendBTC(address,amount){
-        const feeRate = (await this.getfeeRate())* 1e8
+
+        const feePrice = await this.getfeeRate()
+        const feeRate = new BigNumber(feePrice).times(1e8).toNumber()
+        const value = new BigNumber(amount).times(1e8).toNumber()
         const targets = [{
             address,
-            value: amount*1e8
+            value
         }]
         //const froms 
-        const utxos = getAddressUtxo()
+        const utxos = await this.getUtxos()
         const {inputs, outputs, fee} = coinSelect(utxos,targets,feeRate)
+        if (inputs && outputs){
+            //build tx
+            const filledOutputs = outputs.map(output => {
+                if (!output.address) {
+                  output.address = inputs[0].address; // 在这里设置默认值
+                }
+                return output;
+            })
+            const network = bitcoin.networks.testnet
+            const tx = new bitcoin.TransactionBuilder(network)
+            inputs.forEach((e)=>tx.addInput(e.txid,e.vout))
+            filledOutputs.forEach((e)=>tx.addOutput(e.address,e.value))
+
+            //sign tx
+            const froms = this.config.KEYPAIR
+            let signer,priKey
+            for (let i =0;i<inputs.length;i++){
+                priKey = _.find(froms, function(o) { return o.ADDRESS == inputs[i].address})
+                signer = bitcoin.ECPair.fromWIF(priKey.PRIKEY, network)
+                tx.sign(i,signer)
+            }
+            
+            //send tx
+            const rawtx = tx.build().toHex()
+            const hash = await this.sendTx(rawtx)
+            console.log(`Transfer ${amount} BTC to ${address}. Transaction Hash: ${hash.txid}`);
+
+        } else {
+            throw new Error('not enough utxos!!!')
+        }
+
+        
 
     }
 }
+
+module.exports = btcBasic
--- transfer_BTC.js
+++ transfer_BTC.js
@@ -1,2 +1,9 @@
-const bitcoin = require('bitcoinjs-lib');
-const config = require('./configLoad');
+const btcBasic = require('./btcBasic');
+
+const btc = new btcBasic('BTC')
+
+// 从命令行参数中获取充值地址和转账金额
+const address = process.argv[2];
+const transferAmount = parseFloat(process.argv[3]);
+
+btc.sendBTC(address, transferAmount)
