c9622f677575e570578c79c8bbc6d38b82d0ca88
shiyu <zhangshiyu@hashkey.com>
2023-11-28 10:16:53 +0800

    add genaddress

--- transfer_DOGE.js
+++ transfer_DOGE.js
@@ -13,6 +13,8 @@ class dogeBasic extends btcBasic{
 
 const doge = new dogeBasic('DOGE')
 
+// console.log(doge.genAddressByRandom())
+
 // 从命令行参数中获取充值地址和转账金额
 const address = process.argv[2];
 const transferAmount = parseFloat(process.argv[3]);
